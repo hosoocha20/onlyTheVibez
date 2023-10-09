46ae7a5287f18663ffe00f259b7da9d4add51f9e
Hosoo Cha <hosoo.cha@gmail.com>
2023-10-09 19:42:37 +1300

    localhost

--- server/connect.js
+++ server/connect.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-    host: process.env.HOST,
+    host: '0.0.0.0',
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
